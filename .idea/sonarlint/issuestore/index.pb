564404e332941b3fbdac79c877877fd1aa2f31ae
Colin O'Hanlon <cohanlon@MacBook-Pro-2.fios-router.home>
2022-07-29 12:20:16 -0400

    Updated live template and test running setup doc

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+r
+Bdemo/src/main/java/com/tdd/demo/controller/UserInfoController.java,a/9/a90b83ca50b9c7c3c8d1061bcd500e4c0f3c19ef
---
+++ demo/src/test/java/com/tdd/demo/service/AdHocServiceTest.java
@@ -0,0 +1,35 @@
+package com.tdd.demo.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdHocServiceTest {
+
+
+
+    @Spy
+    @InjectMocks
+    AdHocService service;
+
+
+    @Test
+    public void healthCheck() {
+        Assertions.assertTrue(true);
+
+        Assertions.assertNotNull(service);
+        // don't forget to openMocks!
+    }
+
+    @BeforeEach
+    public void init(){
+      MockitoAnnotations.openMocks(this);
+      // consider adding healthcheck method below this init()
+    }
+
+}
